0cc997abd476025de95d873f2033ab048cd1d361
恩得 王 <waynexz@126.com>
2019-09-13 16:01:11 +0800

    bug1

---
+++ 字符的bug/字符的bug/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 字符的bug
+{
+    class Person
+    {
+        //字段
+        private string name;
+        private int _age;
+        private char _gender;
+
+        //属性
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    value = 0;
+                }
+                _age = value;
+            }
+        }
+
+        public char Gender
+        {
+            get
+            {
+                if (_gender != '男' && _gender != '女')
+                {
+                    return _gender = '男';
+                }
+                return _gender;
+            }
+
+            set
+            {
+                _gender = value;
+            }
+        }
+
+        //当你给属性赋值的时候，首先会执行set方法
+        //当你输出属性的值的时候，会执行get方法
+        //public string Name { get => name; set => name = value; }
+
+
+        //方法
+        public void CHLSS()
+        {
+            Console.WriteLine("我叫{0},我今年{1}岁了, 我是{2}生,我可以吃喝拉撒睡哦", this.Name, this.Age, this.Gender);
+
+        }
+    }
+}
---
+++ 字符的bug/字符的bug/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 字符的bug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建Person类的对象
+            Person zhangsan = new Person();  //类的实例化
+            zhangsan.Name = "张三";
+            zhangsan.Age = -23;
+            zhangsan.Gender = '春';
+            zhangsan.CHLSS();
+            Console.ReadKey();
+        }
+    }
+}
